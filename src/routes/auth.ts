8627998255c4c424808021c1cb39208994374ffb
KhaliunGerel <khaliun0122@gmail.com>
2025-11-07 10:03:14 -0500

    feat: added register route

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -16,4 +16,18 @@ router.post("/login", (req, res) => {
   return res.status(200).json({ success: true });
 });
 
+router.post("/register", (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({ error: "Missing fields" });
+  }
+
+  if (password.length < 6) {
+    return res.status(401).json({ error: "Invalid password" });
+  }
+
+  return res.status(200).json({ success: true });
+});
+
 export default router;
